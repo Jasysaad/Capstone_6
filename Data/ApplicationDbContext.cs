110cd6dc0614e39889b2b9bac746ca1a37f9fc1f
jasmine saad <jasmineysaad@gmail.com>
2021-05-01 12:18:03 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone_6.Models;
+
+namespace Capstone_6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Login> Task { get; set; }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_6.Models
+{
+    public class Login
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string Description { get; set; }
+        public DateTime Date { get; set; }
+        public IdentityUser User { get; set; }
+        
+    }
+}
